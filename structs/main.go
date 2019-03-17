71960b22dc0892e6274cc35bff4fa22c2d0562c6
AbsolutelySaurabh <absolutelysaurabh03gmail.com>
2019-03-17 18:56:02 +0530

    structs done(pointers, pass by val, ref, gotchas)

--- structs/main.go
+++ structs/main.go
@@ -6,6 +6,15 @@ type person struct {
 	firstname string
 	lastname  string
 	contact   contactInfo
+
+	/*
+		In this case, we need to define in this way
+		contactInfo : contactInfo{
+			email : "ass",
+			zip : 123,
+		}
+	*/
+	//contactInfo  --> This also works, i.e. just the type if struct and not name;
 }
 
 type contactInfo struct {
@@ -38,4 +47,32 @@ func main() {
 
 	fmt.Printf("%+v", jim)
 
+	//structs with receiver functions
+	jim.print()
+	jim.updateName("Jammy")
+	jim.print()
+
+	/*
+		IN the abve statements, after updating firstname,
+		it doen't changed the original
+
+		Reason : Pass By Value
+
+		Note : But in case of arrays, slices they get passed by reference by default.
+				In this case it'll copy in memoery too but will still point to the same memoery address.
+				So, in case of arrays, slices a "reference" is passed, in case of other data types value is passsed by default.
+	*/
+
+	// using pointers to avoid above
+	jimPointer := &jim
+	jimPointer.updateName("Samuels")
+	jim.print()
+}
+
+func (pointer *person) updateName(newf string) {
+	pointer.firstname = newf
+}
+
+func (p person) print() {
+	fmt.Println(p)
 }
